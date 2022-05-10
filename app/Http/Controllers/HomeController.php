753a343164bd34a7f1cb2a18776ab69065024605
Cristian González <crisgon1065@gmail.com>
2022-05-10 16:09:29 -0600

    Backend consumo de FCM y actualizacion de token

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class HomeController extends Controller
 {
+    public $serverKey = null;
+    public $fcmURL = null;
+
     /**
      * Create a new controller instance.
      *
@@ -14,6 +18,8 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->serverKey = env('SERVER_KEY', '');
+        $this->fcmURL    = env('FCM_URL', '');
     }
 
     /**
@@ -24,5 +30,51 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    //Actualizar el token
+    public function saveToken(Request $request)
+    {
+        auth()->user()->update(['device_token' => $request->token]);
+
+        return response()->json(['El token se ha actualizado correctamente.']);
+    }
+
+    //Envio de notificacion
+    public function sendNotification(Request $request)
+    {
+        $tokens = User::whereNotNull('device_token')->pluck('device_token')->all();
+
+        $data = [
+            "registration_ids" => $tokens,
+            "notification" => [
+                "title" => $request->title,
+                "body"  => $request->body,  
+            ]
+        ];
+
+        $headers = [
+            'Authorization: key='.$this->serverKey,
+            'Content-Type: application/json',
+        ];
+
+        $response = $this->cURL($headers, $data);
+        $message = json_decode($response)->success ? 'Notificación enviada correctamente.' : 'Ocurrió un error al enviar la notificación.';
+
+        return back()->with('status', $message);
+    }
+
+    //Consumo de FCM enviar Push
+    public function cURL($headers, $data) {
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $this->fcmURL);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+        return curl_exec($curl);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/',                 [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/token',           [App\Http\Controllers\HomeController::class, 'saveToken'])->name('token');
+Route::post('/notification',    [App\Http\Controllers\HomeController::class, 'sendNotification'])->name('notification');
